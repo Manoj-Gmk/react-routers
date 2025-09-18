d3389febb53bc110dcc2a1613a1ef85c3b40e2ee
Manoj-Gmk <manojgare528@gmail.com>
2025-09-18 19:07:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-// Importing pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
-
-// React Router DOM imports
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-// Importing global styles
 import "./App.css";
-
-// Importing layouts and components
 import ContactLayout from "./NewRouterMethod/ContactLayout";
 import ContactInfo from "./Components/ContactInfo";
 import ContactForm from "./Components/ContactForm";
@@ -22,15 +15,12 @@ function App() {
   // Creating router configuration using React Router v6's createBrowserRouter
   const router = createBrowserRouter([
     {
-      path: "/", // Root path
-      // Main layout component (e.g., with Navbar)
+      path: "/", 
       element: <NewRouter />,
       children: [
-        { index: true, element: <Home /> }, // Default route ("/") renders Home
-        { path: "products", element: <Products /> }, // "/products" renders Products
-        { path: "about", element: <About /> }, // "/about" renders About page
-
-        // Nested Contact routes using ContactLayout
+        { index: true, element: <Home /> }, 
+        { path: "products", element: <Products /> },
+        { path: "about", element: <About /> }, 
         {
           path: "contact",
           element: <ContactLayout />,
@@ -39,8 +29,6 @@ function App() {
             { path: "contactform", element: <ContactForm /> }, // "/contact/contactform"
           ],
         },
-
-        // Nested Job routes using JobLayout
         {
           path: "joblayout",
           element: <JobLayout />,
@@ -52,14 +40,11 @@ function App() {
             },
           ],
         },
-
         // Fallback route for undefined paths
         { path: "*", element: <NotFound /> },
       ],
     },
   ]);
-
-  // Providing the router to the application
   return <RouterProvider router={router} />;
 }
 
